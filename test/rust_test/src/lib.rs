3b3a3a3c94945de676e34d9206676db029e12f9d
Kristoffer Søholm <k.soeholm@gmail.com>
2021-12-29 21:07:55 +0100

    Add deserialize tests, improve testing framework

--- test/rust_test/src/lib.rs
+++ test/rust_test/src/lib.rs
@@ -9,3 +9,36 @@ pub mod planus_test;
 mod conformance;
 pub mod hexdump;
 mod unit_tests;
+
+#[cfg(test)]
+pub mod tests {
+    use anyhow::Result;
+    use std::path::Path;
+
+    pub fn compare_regenerate_file<P, S, D, R>(
+        path: P,
+        new_val: R,
+        serialize: S,
+        deserialize: D,
+        should_regenerate: bool,
+    ) -> Result<()>
+    where
+        P: AsRef<Path>,
+        D: Fn(Vec<u8>) -> R,
+        S: Fn(R) -> Vec<u8>,
+        R: PartialEq + std::fmt::Debug,
+    {
+        let path = path.as_ref();
+
+        if path.exists() && !should_regenerate {
+            let data = std::fs::read(path)?;
+            let prev_val = deserialize(data);
+            similar_asserts::assert_eq!(prev_val, new_val);
+        } else {
+            let data = serialize(new_val);
+            std::fs::write(path, &data)?;
+        }
+
+        Ok(())
+    }
+}
--- test/rust_test/src/test_template.rs
+++ test/rust_test/src/test_template.rs
@@ -2,6 +2,8 @@
 #[allow(unused_imports)]
 use planus::{SliceWithStartOffset, TableRead, WriteAsOffset};
 #[allow(unused_imports)]
+use std::convert::identity;
+#[allow(unused_imports)]
 use std::{
     fs::{File, OpenOptions},
     io::{Read, Write},
@@ -9,30 +11,21 @@ use std::{
 
 #[test]
 fn test_serialize() {
-    for entry in std::fs::read_dir(FILE_PATH).unwrap() {
+    let should_regenerate = std::env::var("PLANUS_REGENERATE").is_ok();
+    for entry in std::fs::read_dir(format!("{}/{}", FILE_PATH, "serialize")).unwrap() {
         let entry = entry.unwrap();
         let file_path = entry.path();
-        if file_path
-            .extension()
-            .map_or(false, |extension| extension == "json")
+        if !file_path.is_dir()
+            && file_path
+                .extension()
+                .map_or(false, |extension| extension == "json")
         {
             let json = std::fs::read_to_string(&file_path).unwrap();
             let root: Root = serde_json::from_str(&json).unwrap();
 
-            let mut bin_path = file_path.clone();
-            bin_path.set_extension("bin");
-            let data =
-                if let Ok(mut file) = OpenOptions::new().read(true).write(true).open(&bin_path) {
-                    let mut data = Vec::new();
-                    file.read_to_end(&mut data).unwrap();
-                    data
-                } else {
-                    let mut buffer = planus::Buffer::new();
-                    let root = root.prepare(&mut buffer);
-                    let data = buffer.finish(root, None);
-                    File::create(&bin_path).unwrap().write_all(data).unwrap();
-                    data.to_vec()
-                };
+            let mut buffer = planus::Buffer::new();
+            let offset = root.prepare(&mut buffer);
+            let data = buffer.finish(offset, None);
 
             let root_ref = RootRef::from_buffer(
                 SliceWithStartOffset {
@@ -43,7 +36,56 @@ fn test_serialize() {
             )
             .unwrap();
             let root2 = planus::ToOwned::to_owned(root_ref).unwrap();
-            assert_eq!(root, root2);
+            similar_asserts::assert_eq!(root, root2);
+
+            let mut bin_path = file_path.clone();
+            bin_path.set_extension("bin");
+            crate::tests::compare_regenerate_file(
+                &bin_path,
+                data.to_vec(),
+                identity,
+                identity,
+                should_regenerate,
+            )
+            .unwrap();
+        }
+    }
+}
+
+#[test]
+fn test_deserialize() {
+    if let Ok(refs_dir) = std::fs::read_dir(format!("{}/{}", FILE_PATH, "deserialize")) {
+        let should_regenerate = std::env::var("PLANUS_REGENERATE").is_ok();
+        for entry in refs_dir {
+            let entry = entry.unwrap();
+            let file_path = entry.path();
+            if file_path
+                .extension()
+                .map_or(false, |extension| extension == "bin")
+            {
+                let data = std::fs::read(&file_path).unwrap();
+
+                let root_ref = RootRef::from_buffer(
+                    SliceWithStartOffset {
+                        buffer: &data,
+                        offset_from_start: 0,
+                    },
+                    0,
+                )
+                .unwrap();
+
+                let mut debug_path = file_path.clone();
+                debug_path.set_extension("txt");
+
+                crate::tests::compare_regenerate_file(
+                    &debug_path,
+                    format!("{:#?}", root_ref),
+                    |s| s.into_bytes(),
+                    |v| String::from_utf8(v).unwrap(),
+                    should_regenerate,
+                )
+                .unwrap();
+            }
         }
     }
 }
