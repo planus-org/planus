f003dffc15d8ebfe444fef3d4dcfc1763f87a87a
Kristoffer Søholm <k.soeholm@gmail.com>
2021-12-29 17:55:03 +0100

    Add support for multiple tests per fbs file

--- test/rust_test/src/test_template.rs
+++ test/rust_test/src/test_template.rs
@@ -9,30 +9,41 @@ use std::{
 
 #[test]
 fn test_serialize() {
-    let json = std::fs::read_to_string(format!("{}.json", FILE_PATH)).unwrap();
-    let root: Root = serde_json::from_str(&json).unwrap();
+    for entry in std::fs::read_dir(FILE_PATH).unwrap() {
+        let entry = entry.unwrap();
+        let file_path = entry.path();
+        if file_path
+            .extension()
+            .map_or(false, |extension| extension == "json")
+        {
+            let json = std::fs::read_to_string(&file_path).unwrap();
+            let root: Root = serde_json::from_str(&json).unwrap();
 
-    let bin_path = format!("{}.bin", FILE_PATH);
-    let data = if let Ok(mut file) = OpenOptions::new().read(true).write(true).open(&bin_path) {
-        let mut data = Vec::new();
-        file.read_to_end(&mut data).unwrap();
-        data
-    } else {
-        let mut buffer = planus::Buffer::new();
-        let root = root.prepare(&mut buffer);
-        let data = buffer.finish(root, None);
-        File::create(&bin_path).unwrap().write_all(data).unwrap();
-        data.to_vec()
-    };
+            let mut bin_path = file_path.clone();
+            bin_path.set_extension("bin");
+            let data =
+                if let Ok(mut file) = OpenOptions::new().read(true).write(true).open(&bin_path) {
+                    let mut data = Vec::new();
+                    file.read_to_end(&mut data).unwrap();
+                    data
+                } else {
+                    let mut buffer = planus::Buffer::new();
+                    let root = root.prepare(&mut buffer);
+                    let data = buffer.finish(root, None);
+                    File::create(&bin_path).unwrap().write_all(data).unwrap();
+                    data.to_vec()
+                };
 
-    let root_ref = RootRef::from_buffer(
-        SliceWithStartOffset {
-            buffer: &data,
-            offset_from_start: 0,
-        },
-        0,
-    )
-    .unwrap();
-    let root2 = planus::ToOwned::to_owned(root_ref).unwrap();
-    assert_eq!(root, root2);
+            let root_ref = RootRef::from_buffer(
+                SliceWithStartOffset {
+                    buffer: &data,
+                    offset_from_start: 0,
+                },
+                0,
+            )
+            .unwrap();
+            let root2 = planus::ToOwned::to_owned(root_ref).unwrap();
+            assert_eq!(root, root2);
+        }
+    }
 }
