a17975a037cd795a184397cea653530546680389
Tethys Svensson <freaken@freaken.dk>
2022-03-22 13:54:04 +0100

    Fix clippy (#95)
    * Fix clippy warnings
    
    * The svg got updated for some reason

--- planus-cli/src/app/gen_completions.rs
+++ planus-cli/src/app/gen_completions.rs
@@ -1,9 +1,9 @@
-use clap::{AppSettings, IntoApp, Parser};
+use clap::{IntoApp, Parser};
 use clap_complete::{generate, Shell};
 
 /// Generate shell completion scripts
 #[derive(Parser)]
-#[clap(setting = AppSettings::ArgRequiredElseHelp)]
+#[clap(arg_required_else_help = true)]
 pub struct Command {
     /// Which shell to generate completions for
     #[clap(arg_enum)]
@@ -14,7 +14,7 @@ impl Command {
     pub fn run(self, _options: super::AppOptions) {
         generate(
             self.shell,
-            &mut super::App::into_app(),
+            &mut super::App::command(),
             "planus",
             &mut std::io::stdout(),
         );
