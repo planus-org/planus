2404deeb52943ebf209dbf7b2d5d0476863f8514
Tethys Svensson <freaken@freaken.dk>
2022-07-03 12:14:52 +0200

    Use a flake.nix instead of a shell.nix

---
+++ flake.nix
@@ -0,0 +1,50 @@
+{
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-22.05";
+    rust-overlay.url = "github:oxalica/rust-overlay";
+  };
+
+  outputs = { self, nixpkgs, rust-overlay, ... }:
+    let
+      system = "x86_64-linux";
+      overlays = [ (import rust-overlay) ];
+      pkgs = import nixpkgs {
+        inherit system overlays;
+      };
+
+    in
+    {
+      devShell."${system}" = with pkgs;
+        mkShell {
+          nativeBuildInputs = [
+            # nix develop shells will by default include a bash in the $PATH,
+            # however this bash will be a non-interactive bash. The deviates from
+            # how nix-shell works. This fix was taken from:
+            #    https://discourse.nixos.org/t/interactive-bash-with-nix-develop-flake/15486
+            bashInteractive
+
+            (rust-bin.stable.latest.default.override {
+              extensions = [ "rust-src" ];
+              targets = [ "x86_64-unknown-linux-gnu" ];
+            })
+
+            bacon
+            flatbuffers
+            cargo-make
+            graphviz
+          ];
+
+          RUST_SRC_PATH = "${rust.packages.stable.rustPlatform.rustLibSrc}";
+
+          shellHook = ''
+            # nix develop shells will by default overwrite the $SHELL variable with a
+            # non-interactive version of bash. The deviates from how nix-shell works.
+            # This fix was taken from:
+            #    https://discourse.nixos.org/t/interactive-bash-with-nix-develop-flake/15486
+            #
+            # See also: nixpkgs#5131 nixpkgs#6091
+            export SHELL=${bashInteractive}/bin/bash
+          '';
+        };
+    };
+}
