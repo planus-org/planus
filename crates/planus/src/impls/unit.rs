9a194bf89c150a01f0a08e190c7611357f5939a9
Tethys Svensson <freaken@freaken.dk>
2023-12-22 10:57:28 +0100

    Try again

--- crates/planus/src/impls/unit.rs
+++ crates/planus/src/impls/unit.rs
@@ -6,7 +6,7 @@ use crate::{
 
 impl<P: Primitive> WriteAsPrimitive<P> for Void {
     #[inline]
-    #[allow(clippy::uninhabited_reference)]
+    #[allow(clippy::uninhabited_references)]
     fn write<const N: usize>(&self, _cursor: Cursor<'_, N>, _buffer_position: u32) {
         match *self {}
     }
