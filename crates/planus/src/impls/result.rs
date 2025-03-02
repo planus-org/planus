91972bfceae91ba83fe10e8dc4900e0920257140
Kristoffer Søholm <k.soeholm@gmail.com>
2025-03-02 12:44:00 +0100

    Remove dead code

--- crates/planus/src/impls/result.rs
+++ crates/planus/src/impls/result.rs
@@ -1,19 +1,5 @@
 use crate::traits::{VectorRead, VectorReadInner};
 
-/*
-impl<T: ToOwned, E> ToOwned for core::result::Result<T, E>
-where
-    errors::Error: From<E>,
-{
-    type Value = T::Value;
-
-    #[inline]
-    fn to_owned(self) -> crate::Result<Self::Value> {
-        self?.to_owned()
-    }
-}
- */
-
 impl<'buf, T: VectorReadInner<'buf>, E: 'buf> VectorRead<'buf> for Result<T, E>
 where
     E: core::convert::From<T::Error>,
