108030da970fb92726df53f435fc808c9c27c150
Tethys Svensson <freaken@freaken.dk>
2025-08-26 11:40:06 +0200

    Release version 1.2.0

--- examples/rust/src/monster_generated.rs
+++ examples/rust/src/monster_generated.rs
@@ -1,6 +1,6 @@
 pub use root::*;
 
-const _: () = ::planus::check_version_compatibility("planus-1.1.1");
+const _: () = ::planus::check_version_compatibility("planus-1.2.0");
 
 /// The root namespace
 ///
